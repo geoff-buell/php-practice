a776a41cccdbda95d8dcb892af4de7dd18f32708
Geoffrey Buell <info@geoffreybuell.com>
2021-08-05 10:54:42 -0400

    Added form validations

--- form-handling.php
+++ form-handling.php
@@ -34,12 +34,20 @@
         $nameErr = "Name is required";
       } else {
         $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+          $nameErr = "Only letters and white space allowed";
+        }
       }
 
       if (empty($_POST["email"])) {
         $emailErr = "Email is required";
       } else {
         $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $emailErr = "Invalid email format";
+        }
       }
       
     }
