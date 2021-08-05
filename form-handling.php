a8269f5478b1acaf44011ea12d9ba06715a8e2be
Geoffrey Buell <info@geoffreybuell.com>
2021-08-05 10:36:17 -0400

    Practiced form security and validation

--- form-handling.php
+++ form-handling.php
@@ -14,7 +14,31 @@
     <input type="submit">
   </form> -->
 
-  <form action="form-handling.php" method="post">
+  <!-- <form action="form-handling.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+  </form> -->
+
+  <?php 
+
+    $name = $email = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = test_input($_POST["name"]);
+      $email = test_input($_POST["email"]);
+    }
+
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+
+  ?>
+
+  <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post">
     Name: <input type="text" name="name"><br>
     E-mail: <input type="text" name="email"><br>
     <input type="submit">
