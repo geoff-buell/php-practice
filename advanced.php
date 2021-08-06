beac74d9b442b22b5508b80e55f272a554d70fe5
Geoffrey Buell <info@geoffreybuell.com>
2021-08-06 08:53:32 -0400

    Studied date and time

---
+++ advanced.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Advanced</title>
+</head>
+<body>
+  
+  <?php
+    // Date and time
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l") . "<br>";
+
+    echo "The time is " . date("h:i:sa") . "<br>";
+
+    date_default_timezone_set("America/New_York");
+    echo "The time is " . date("h:i:sa") . "<br>";
+
+    // mktime(hour, minute, second, month, day, year)
+    $d = mktime(11, 14, 54, 8, 12, 2014);
+    echo "Created date is " . date("Y-m-d h:i:sa", $d) . "<br>";
+
+    // strtotime(time, now)
+    $d = strtotime("10:30pm April 15 2014");
+    echo "Created date is " . date("Y-m-d h:i:sa", $d) . "<br>";
+  
+    $d = strtotime("tomorrow");
+    echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+    $d = strtotime("next Saturday");
+    echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+    $d = strtotime("+3 Months");
+    echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+    // outputs next 6 Saturdays
+    $startDate = strtotime("Saturday");
+    $endDate = strtotime("+6 weeks", $startDate);
+
+    while ($startDate < $endDate) {
+      echo date("M d", $startDate) . "<br>";
+      $startDate = strtotime("+1 week", $startDate);
+    }
+
+    // outputs number of days until 4th of July
+    $d1 = strtotime("July 04");
+    $d2 = ceil(($d1-time())/60/60/24);
+    echo "There are " . $d2 . "days until 4th of July.";
+
+  ?>
+
+</body>
+</html>
