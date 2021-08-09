61a3910fe4d421b7531ae5a2e3b0641f036a8fcc
Geoffrey Buell <info@geoffreybuell.com>
2021-08-09 10:12:04 -0400

    Messed around with cookies

--- advanced.php
+++ advanced.php
@@ -1,4 +1,16 @@
 <!DOCTYPE html>
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+<?php
+// modify by setting cookie again
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+// delete by setting cookie expiration date in the past
+?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -104,6 +116,27 @@
     $txt = "Minnie Mouse\n";
     fwrite($myFile, $txt);
     fclose($myFile);
+
+    echo "<br>";
+  ?>
+
+  <?php
+    if(!isset($_COOKIE[$cookie_name])) {
+      echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+      echo "Cookie '" . $cookie_name . "' is set!<br>";
+      echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+
+    echo "<br>";
+  ?>
+
+  <?php
+    if(count($_COOKIE) > 0) {
+      echo "Cookies are enabled.";
+    } else {
+      echo "Cookies are disabled.";
+    }
   ?>
 
 </body>
