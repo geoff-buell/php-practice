30ed3fc3a70a7e2993b041a3ae422b45d7f98b27
Geoffrey Buell <info@geoffreybuell.com>
2021-08-10 10:39:17 -0400

    Worked on exceptions

--- advanced.php
+++ advanced.php
@@ -289,6 +289,39 @@
     foreach($arr as $key => $value) {
       echo $key . " => " . $value . "<br>";
     }
+
+    function divide($dividend, $divisor) {
+      if ($divisor == 0) {
+        throw new Exception("Division by zero");
+      }
+      return $dividend / $divisor;
+    }
+    // echo divide(5, 0); this will result in a fatal error 
+
+    try {
+      echo divide(5, 0);
+    } catch(Exception $e) {
+      echo "Unable to divide. <br>";
+    }
+
+    try {
+      echo divide(5, 0);
+    } catch(Exception $e) {
+      echo "Unable to divide. ";
+    } finally {
+      echo "Process complete. <br>";
+    }
+
+    try {
+      echo divide(5, 0);
+    } catch(Exception $ex) {
+      $code = $ex->getCode();
+      $message = $ex->getMessage();
+      $file = $ex->getFile();
+      $line = $ex->getLine();
+      echo "Exception thrown in $file on line $line: [Code $code]
+      $message";
+    }
   ?>
 
 </body>
