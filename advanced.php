ad43f5d847826af018e1bb567df30c56c50dda27
Geoffrey Buell <info@geoffreybuell.com>
2021-08-10 09:26:54 -0400

    Worked on more advanced filters

--- advanced.php
+++ advanced.php
@@ -196,6 +196,36 @@
     } else {
       echo "$url is an invalid url. <br>";
     }
+
+    $int = 122;
+    $min = 1;
+    $max = 100;
+    if (filter_var($int, FILTER_VALIDATE_INT, array("options" => array("min_range" => $min, "max_range" => $max))) === false) {
+      echo "Variable value is not within the legal range. <br>";
+    } else {
+      echo "Variable value is within the legal range. <br>";
+    }
+
+    $ip = "2001:0db8:85a3:08d3:1319:8a2e:0370:7334";
+    if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) === false) {
+      echo "$ip is a valid IPv6 address <br>";
+    } else {
+      echo "$ip is not a valid IPv6 address <br>";
+    }
+
+    $url = "https://www.website.com";
+    if (!filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_QUERY_REQUIRED) === false) {
+      echo "$url is a valid url with a query string. <br>";
+    } else {
+      echo "$url is not a valid url with a query string. <br>";
+    } 
+
+    $str = "<h1>Hello WorldÆØÅ!</h1>";
+    echo "$str <br>";
+    $newStr = filter_var($str, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    echo "$newStr <br>";
+
+    
   ?>
 
 </body>
