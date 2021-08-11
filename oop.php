f23185902e04339e56dd7364e52313ec0a6b9517
Geoffrey Buell <info@geoffreybuell.com>
2021-08-11 10:15:21 -0400

    Worked through abstract classes

--- oop.php
+++ oop.php
@@ -164,5 +164,69 @@
     $goodbye = new Goodbye();
     $goodbye->byebye();
   ?>
+
+  <?php
+    abstract class Car {
+      public $name;
+      public function __construct($name) {
+        $this->name = $name;
+      }
+      abstract public function intro() : string;
+    }
+
+    class Audi extends Car {
+      public function intro() : string {
+        return "Choose German quality! I'm an $this->name!";
+      }
+    }
+
+    class Volvo extends Car {
+      public function intro() : string {
+        return "Proud to be Swedish! I'm a $this->name!";
+      }
+    }
+
+    class Citroen extends Car {
+      public function intro() : string {
+        return "Frence extravagance! I'm a $this->name!";
+      }
+    }
+
+    $audi = new Audi("Audi");
+    echo $audi->intro() . "<br>";
+
+    $volvo = new Volvo("Volvo");
+    echo $volvo->intro() . "<br>";
+
+    $citroen = new Citroen("Citroen");
+    echo $citroen->intro() . "<br>";
+  ?>
+
+  <?php
+    abstract class ParentClass {
+      // abstract method with an argument
+      abstract protected function prefixName($name);
+    }
+
+    class ChildClass extends ParentClass {
+      // the child class may define optional arguments that are not in the parent's abstract method
+      public function prefixName($name, $separator = ".", $greet = "Dear") {
+        if ($name == "John Doe") {
+          $prefix = "Mr";
+        } elseif ($name == "Jane Doe") {
+          $prefix = "Mrs";
+        } else {
+          $prefix = "";
+        }
+        return "{$greet} {$prefix}{$separator} {$name}";
+      }
+    }
+
+    $class = new ChildClass;
+    echo $class->prefixName("John Doe");
+    echo "<br>";
+    echo $class->prefixName("Jane Doe");
+    echo "<br>";
+  ?>
 </body>
 </html>
